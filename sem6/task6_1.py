28a54bee4e3639ac2db5c2e0f04e00ad5ea06d94
Semen <semgenomsk@yandex.ru>
2023-08-13 22:47:31 +0600

    add HomW task sem6

--- sem6/task6_1.py
+++ sem6/task6_1.py
@@ -1,16 +1,44 @@
 """
-Напишите функцию, которая принимает на вход строку —
-абсолютный путь до файла. Функция возвращает кортеж из трёх
-элементов: путь, имя файла, расширение файла
+Создайте модуль и напишите в нём функцию, которая получает на вход дату в формате DD.MM.YYYY
+Функция возвращает истину, если дата может существовать или ложь, если такая дата невозможна.
+Для простоты договоримся, что год может быть в диапазоне [1, 9999].
+Весь период (1 января 1 года - 31 декабря 9999 года) действует
+Григорианский календарь.
+Проверку года на високосность вынести в отдельную защищённую функцию.
 """
 
+def dat(st):
+    day, month, year = map(int, (st.split(".")))
 
-import os
+    if year in range(1, 10000) and month in range(1, 13) and day in range(1,
+                                                                          32):
+        if year % 400 == 0 or year % 4 == 0 and year % 100 != 0 and month == 2:
+            if day <= 29:
+                return True
+            else:
+                return False
+        if month in [1, 3, 5, 7, 8, 10, 12]:
+            if day <= 31:
+                return True
+            else:
+                return False
+        elif month == 2:
+            if day <= 28:
+                return True
+            else:
+                return False
+        else:
+            if day <= 30:
+                return True
+            else:
+                return False
+    else:
+        return False
 
-def info_path(path):
-    file_path, file_extension = os.path.splitext(path)
-    dirc, file_name = os.path.split(file_path)
-    return (dirc, file_name, file_extension)
+def _visokostnosn(year):
+    vis = ("Високосный" if year % 400 == 0 or year % 4 == 0 and year % 100 != 0 else "Невисокосный")
+    return vis
 
 
-print(info_path("D:\Общая папка\GeekBrains\погружение в Python\Установка среды разработки.rar"))
+print(dat("23.04.2023"))
+print(_visokostnosn(2023))
--- sem6/task6_2.py
+++ sem6/task6_2.py
@@ -1,14 +1,48 @@
 """
-Напишите однострочный генератор словаря, который принимает
-на вход три списка одинаковой длины: имена str, ставка int,
-премия str с указанием процентов вида «10.25%». В результате
-получаем словарь с именем в качестве ключа и суммой
-премии в качестве значения. Сумма рассчитывается
-как ставка умноженная на процент премии
+Создайте модуль и напишите в нём функцию,
+которая получает на вход дату в формате DD.MM.YYYY
+Функция возвращает истину, если дата может существовать или ложь, если такая дата невозможна.
+Для простоты договоримся, что год может быть в диапазоне [1, 9999].
+Весь период (1 января 1 года - 31 декабря 9999 года) действует
+Григорианский календарь.
+Проверку года на високосность вынести в отдельную защищённую функцию.
+Запуск в терминале с передачей даты на проверку!!!!!
 """
+from sys import argv
 
-names = ["semen", "stavk", "premi"]
-salary = [1000, 1500, 2000]
-bonus = ["10.15%", "9.25%", "10.50%"]
+def dat(st):
+    day, month, year = map(int, (st.split(".")))
 
-print({name: salary * (1 + float(bonus.strip('%')) / 100) for name, salary, bonus in zip(names, salary, bonus)})
+    if year in range(1, 10000) and month in range(1, 13) and day in range(1,
+                                                                          32):
+        if year % 400 == 0 or year % 4 == 0 and year % 100 != 0 and month == 2:
+            if day <= 29:
+                return True
+            else:
+                return False
+        if month in [1, 3, 5, 7, 8, 10, 12]:
+            if day <= 31:
+                return True
+            else:
+                return False
+        elif month == 2:
+            if day <= 28:
+                return True
+            else:
+                return False
+        else:
+            if day <= 30:
+                return True
+            else:
+                return False
+    else:
+        return False
+
+def _visokostnosn(year):
+    vis = ("Високосный" if year % 400 == 0 or year % 4 == 0 and year % 100 != 0 else "Невисокосный")
+    return vis
+
+"""обязательно ввести параметры"""
+
+print(dat(argv[1]))
+print(_visokostnosn(int(argv[1].split(".")[2])))
